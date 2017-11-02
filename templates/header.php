2dfaa96fbe082d75cb2dd00278e6f15a508adaa0
danielleklaasen <danielleklaasen@gmail.com>
2017-11-02 15:00:36 +0100

    updating sass node module, speed optimization

--- templates/header.php
+++ templates/header.php
@@ -10,12 +10,8 @@
     <meta name="keywords" content="web, developer, full, stack, front, back, end, amsterdam, haarlem, portfolio, design">
     <link rel="author" href="https://plus.google.com/109358922196162573475" />
     <link rel="canonical" href="http://www.danielleklaasen.com" />
-    <!-- loading google fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Cardo:400,400i|Pathway+Gothic+One" rel="stylesheet">
-    <!-- normalize -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
     <!-- style -->
     <link rel="stylesheet" href="dist/styles/main.css">
-    <script src="https://use.fontawesome.com/802c181f5d.js"></script>
+   <!-- <script src="https://use.fontawesome.com/802c181f5d.js"></script>-->
 </head>
 <body>
